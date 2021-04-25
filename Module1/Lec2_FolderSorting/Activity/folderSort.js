b6e94e66e6517f76fd2a503679bd450a7a736507
Amit-1998 <amitkumar654521@gmail.com>
2021-04-25 15:38:29 +0530

    FinallyDone FolderSorting

--- Module1/Lec2_FolderSorting/Activity/folderSort.js
+++ Module1/Lec2_FolderSorting/Activity/folderSort.js
@@ -1,4 +1,4 @@
-let fs = require("fs");
+let fs = require("fs"); //import fs Module
 let extensionsMapping = require("./util.js");
 
 /*
@@ -12,9 +12,11 @@ let extensionsMapping = require("./util.js");
     
 }
 */
-let testFolderPath = "./Downloads";
+let testFolderPath = "./Downloads";  // Folder ka Path On which we are working
+// getAllFiles ka kaam
 let allFiles = fs.readdirSync(testFolderPath);
 
+// Loop on each File
 for(let i=0; i<allFiles.length; i++){
     sortFile(allFiles[i]);
 }
@@ -26,16 +28,26 @@ function sortFile(file){
 }
 
 function checkExtensionFolder(extension){
+    // suppose extension is .doc
 
     let extensionFolderName = testFolderPath;
     for(let key in extensionsMapping){
        let extensions = extensionsMapping[key];
+                       //     for(let i=0; i<extensions.length; i++){
+                    //         if(extensions[i]==extension){
+                    //              extensionFolderName = testFolderPath+        
+                    //         }
+                    //    }
        if(extensions.includes(extension)){
           extensionFolderName = extensionFolderName+"/"+key;
           break;
        }
     }
 
+    //here we would have extensionFolderName = Documents
+    // "./Downloads"
+    
+    // "./Downloads/Documents"  Now we have to check that is this Path exists?
     let isFolderExist = fs.existsSync(extensionFolderName);
     if(!isFolderExist){
       fs.mkdirSync(extensionFolderName);
@@ -48,7 +60,9 @@ function moveFile(file,extensionFolderName){
     let srcPath = testFolderPath+"/"+file;
     let destPath = extensionFolderName+"/"+file;
 
+    //copy file from source path to detination path
     fs.copyFileSync(srcPath,destPath);
+    //then delete file from the source path
     fs.unlinkSync(srcPath);
 }
 
