3bcee23c4fac38552791ff69a41c502808977b03
Amit-1998 <amitkumar654521@gmail.com>
2021-06-15 22:53:59 +0530

    done till changes in color of tickets-container on selecting filter

---
+++ Module2/Ticket_App/activity/script.js
@@ -0,0 +1,15 @@
+let allFilters = document.querySelectorAll(".filter");
+let ticketsContainer = document.querySelector(".tickets-container");
+
+for (let i = 0; i < allFilters.length; i++) {
+    allFilters[i].addEventListener("click", selectFilter);
+}
+
+function selectFilter(e) {
+    let selectedFilter = e.target.classList[1];
+    if (ticketsContainer.classList.length > 1) {
+        ticketsContainer.classList.remove(ticketsContainer.classList[1]);
+    }
+    ticketsContainer.classList.add(selectedFilter);
+
+}
