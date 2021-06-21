23c8476cb6c09b79dac7f9ae10d34db1e0670c4e
Amit-1998 <amitkumar654521@gmail.com>
2021-06-21 14:55:16 +0530

    canvas started

---
+++ Module2/WhiteBoard/activity/script.js
@@ -0,0 +1,19 @@
+let canvas = document.querySelector("#canvas");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight - 100;
+
+window.addEventListener("resize", function(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight - 100;
+});
+
+// canvas drawing gets erased on window resize ???
+
+// a context object which provides fun for 2d drawing
+let ctx = canvas.getContext("2d");
+console.log(ctx);
+
+// for demo only
+// ctx.fillStyle = "yellow";
+// ctx.fillRect(10,10,150,100);
