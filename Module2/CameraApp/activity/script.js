088bd9eea27d00ed58655200b145af033c390079
Amit-1998 <amitkumar654521@gmail.com>
2021-06-26 11:30:36 +0530

    video feed on

--- Module2/CameraApp/activity/script.js
+++ Module2/CameraApp/activity/script.js
@@ -1,10 +1,14 @@
 let videoElement = document.querySelector("video");
 
 let constraint = { video: true };
+
+//for opening prompt to ask for access
+// navigator.mediaDevices.getUserMedia => ask permission for access your webcam
 navigator.mediaDevices.getUserMedia(constraint).then(function(mediaStream) {
-    // console.log(mediaStream);
-    videoElement.srcObject = mediaStream;
+    // console.log(mediaStream); gives us mediaStream naam ka oject which have some functions
+    videoElement.srcObject = mediaStream; // a MediaStream from a camera is assigned to a created <video> element.
   })
   .catch(function (error) {
     console.log(error);
   });
+  
