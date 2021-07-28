d31ef30240a7884763c5eb6797bb3e003e241e05
Amit-1998 <amitkumar654521@gmail.com>
2021-07-28 17:12:26 +0530

    chat app => Socket se leave join done

---
+++ Module2/ChatApp/server/public/socket.js
@@ -0,0 +1,20 @@
+// it will send a message to server
+// this is event based means is event par ye message send kara hai
+socket.emit("userconnected", username); // we provide "userconnected" as a event name
+
+socket.on("userDisconnected", function(username){
+    let leaveDiv = document.createElement("div");
+    leaveDiv.classList.add("chat");
+    leaveDiv.classList.add("leave");
+    leaveDiv.textContent = `${username} left chat`;
+    chatWindow.append(leaveDiv);
+})
+
+// Listner of event "join"
+socket.on("join", function(username){
+     let joinDiv = document.createElement("div");
+     joinDiv.classList.add("chat");
+     joinDiv.classList.add("join");
+     joinDiv.textContent = `${username} joined chat`;
+     chatWindow.append(joinDiv);
+})
