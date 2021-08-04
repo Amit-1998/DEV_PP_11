0fd0098cade65bcd00e2b70118deab47aba8532f
Amit-1998 <amitkumar654521@gmail.com>
2021-08-04 14:42:18 +0530

    Homework => MyComp bnaya

---
+++ Module3/React/demo/src/MyComp.js
@@ -0,0 +1,17 @@
+// 3 chise chahiye => h1, p, ul
+
+let MyComp = ()=>{ //component name(MyComp) == File name (MyComp.js) 
+
+    return(
+        <div>
+            <h1>This is h1 tag</h1>
+            <p>This is p tag</p>
+            <ul>
+                <li>This is li1</li>
+                <li>This is li2</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyComp;
--- Module3/React/demo/src/index.js
+++ Module3/React/demo/src/index.js
@@ -4,11 +4,17 @@ import React from 'react'; // this library contains component bnane ka logic (cr
 import ReactDOM from 'react-dom'; //shadow DOM vaala logic i.e render logic //un components ko browser par dikhana kaisa hai uska logic is library mein hota hai
 
 // App is component we have imported
-import App from './App'; // ye App ek component hai jo by default hame mila hua hai jo ki yha import kar rkha hai 
+// import App from './App'; // ye App ek component hai jo by default hame mila hua hai jo ki yha import kar rkha hai 
+
+import MyComp from "./MyComp"; // exactly same name MyComp 
 
 ReactDOM.render(
-                  <App />, // ek Component
+                //   <App />, // ek Component
+                  <MyComp/>,
                   document.getElementById('root') // aur ek jagah jha us component ko dikhana hai
                     // yha par document us index.html(inside public folder) file ko indicate karta hai
                     // ye index.html file se id="root" vaale div ko laata hai aur usme App vaale component ko daal deta hai
                 );
+
+// React.render() sirf ek hi component leta hai                
+// ReactDOM.render() function mein sirf ek hi component lga sakte hai ya pass kar sakte hai
