f4d2ca189f9c20011628050fa126328f03639e33
Amit-1998 <amitkumar654521@gmail.com>
2021-08-04 22:24:08 +0530

    Todo app starts : li's lga kar dekhe

--- Module3/TodoApp/todoapp/src/App.js
+++ Module3/TodoApp/todoapp/src/App.js
@@ -1,9 +1,23 @@
-let App = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+import React from "react";
+
+class App extends React.Component{
+  
+    state = {
+        tasks: ["make coffee", "make notes", "go for a jog","new task"]
+    }
+
+     render = () => {
+         return (
+            <ul>
+                {
+                    this.state.tasks.map((el) => {
+                        return <li>{el}</li>;
+                    })
+                }
+            </ul>
+         );    
+     }
+  
 }
 
 export default App;
