2d6072646a2fcec7b5303b818806c788c3e88f1f
Amit-1998 <amitkumar654521@gmail.com>
2021-08-04 14:29:33 +0530

    App Component ke baare mein sikha

--- Module3/React/demo/src/App.js
+++ Module3/React/demo/src/App.js
@@ -2,10 +2,17 @@ let App = () => {
   return (
     <div >
          <h1>This is Our first react app.</h1>
+         <p>Some other html</p>
     </div>
   );
 };
 
 export default App;
 
+// by Convention => component ka name(App) and file ka name(App.js) same hota hai
+// react enforces us ki jo component ka name hai vo Capital Letter se Start ho
+
 // ye App pura ek Component hai jo yha se export kar rkha hai
+// ye App ek functional component hai kuki ismein ek arrow function hai
+// Note :
+// => Functional Component jo bhi chis return karta hai vo chis UI par shown hoti hai 
