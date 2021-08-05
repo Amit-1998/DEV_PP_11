b5daeb5f863ed87cc52ba088cd83ecebeb79f00f
Amit-1998 <amitkumar654521@gmail.com>
2021-08-05 17:06:52 +0530

    pehle khud try kara

--- Module3/PracticeTests/test1/src/App.js
+++ Module3/PracticeTests/test1/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    
+    state = {
+        operand1 : 0,
+        operand2 : 0,
+        operator : '',
+        result : 0
+    }
+
+    firstNo= (value)=>{
+      this.setState({operand1:value,currentOP:value});
+      console.log(this.state.inputVal1);
+    }
+    SecNo= (value)=>{
+      await this.setState({inputVal2:value,currentOP:this.state.inputVal1+this.state.currentOperation+value});
+      console.log(this.state.inputVal2);
+    }
+    
+    changeOptr=async (optr)=>{
+      await this.setState({currentOperation:optr,currentOP:this.state.inputVal1+optr});
+      console.log(this.state.currentOperation);
+    }
+
+    render = ()=>{
+         return(
+              <div className = "NokiaScreen">
+                    <div className="Result">
+                        <p>{this.state.result}</p>
+                    </div>
+                    
+                    <Operators />
+                    <Input firstNo= {this.inputV1} SecNo= {this.inputV2} val1={this.state.inputVal1} val2={this.state.inputVal2} op={this.state.currentOperation}/>
+              </div>
+         );
+    }
 }
 
 export default App;
---
+++ Module3/PracticeTests/test1/src/Operators.jsx
@@ -0,0 +1,12 @@
+let Operators = ()=>{
+
+    return(
+         <div className="OperatorsButtons">
+              <button onClick = {(e)=>{}}>+</button>
+              <button>-</button>
+              <button>/</button>
+              <button>*</button>
+              <button>=</button>
+         </div>
+    )
+}
