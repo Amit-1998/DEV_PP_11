974a57301582dbd337584b92031bcdad7a385c7b
Amit-1998 <amitkumar654521@gmail.com>
2021-08-10 17:32:57 +0530

    just print the order of stages

--- Module3/MoviesApp/movieapp/src/App.js
+++ Module3/MoviesApp/movieapp/src/App.js
@@ -1,9 +1,22 @@
-let App = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+import React from "react";
+
+class App extends React.Component{
+    
+    constructor(props){ // constructor mein props milte hi hai syntax hai aisa halaki props mein undefine aayega, Although App khud ek Parent Comp hai 
+       
+        super(props); // super ye Uper React.Component vaali class hogi
+        console.log("stage1");
+        this.state = {}; // this means ye jo App comp hai ismein state bna do, pehle ham state ko bhar bnate they,ab bhar nhi bnayenge constructor creation phase mein call hota hai to creation phase mein hi bna denge 
+    }
+ 
+    componentDidMount(){
+       console.log("stage 3 ");
+    }
+
+    render = ()=>{
+        console.log("stage 2");
+        return <div></div>;
+    }
 }
 
 export default App;
