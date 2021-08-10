79d0b2c761ce66681fa3cb0fac3ee4ce5b25ea05
Amit-1998 <amitkumar654521@gmail.com>
2021-08-10 17:44:06 +0530

    All stages explained

--- Module3/MoviesApp/movieapp/src/App.js
+++ Module3/MoviesApp/movieapp/src/App.js
@@ -5,17 +5,34 @@ class App extends React.Component{
     constructor(props){ // constructor mein props milte hi hai syntax hai aisa halaki props mein undefine aayega, Although App khud ek Parent Comp hai 
        
         super(props); // super ye Uper React.Component vaali class hogi
-        console.log("stage1");
-        this.state = {}; // this means ye jo App comp hai ismein state bna do, pehle ham state ko bhar bnate they,ab bhar nhi bnayenge constructor creation phase mein call hota hai to creation phase mein hi bna denge 
+        console.log("constructor was called");
+        this.state = {  // this means ye jo App comp hai ismein state bna do, pehle ham state ko bhar bnate they,ab bhar nhi bnayenge constructor creation phase mein call hota hai to creation phase mein hi bna denge 
+            on: false
+        }; 
     }
  
     componentDidMount(){
-       console.log("stage 3 ");
+       console.log("component did mount was called");
+    }
+
+    componentDidUpdate(){
+        console.log("component did update was called");
     }
 
     render = ()=>{
-        console.log("stage 2");
-        return <div></div>;
+        console.log("render was called");
+        return (
+             <div>
+                  <button onClick={ ()=>{
+                      if(this.state.on){
+                          this.setState( {on: false} );
+                      }
+                      else{
+                         this.setState( {on: true} );
+                      }
+                  } }>Click</button> 
+             </div>
+        );
     }
 }
 
