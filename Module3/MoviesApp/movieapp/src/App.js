0f2084f8bfcdb819f34f7e3c83a7539f971abc00
Amit-1998 <amitkumar654521@gmail.com>
2021-08-12 15:21:21 +0530

    Navbar & Filters(ismein AllGenre and other names) added

--- Module3/MoviesApp/movieapp/src/App.js
+++ Module3/MoviesApp/movieapp/src/App.js
@@ -1,27 +1,51 @@
 import React from "react";
+import Filter from "./Filter";
+import Navbar from "./Navbar";
 
 class App extends React.Component {
-  
-  componentDidMount(){ // data ek baar hi laana hai to hum data compDidMount() mein likhenge
-      // I will get data here
-      // we are in context of FrontEnd side => all Browsers exposed some functionalities like fetch()
-      // fetch() => which takes the link of file/API jha se data mangvaana hai
-      // fetch() is a async function
-      let f = async () => {
-          // let result = await fetch("http://localhost:4000/movies"); //API link jha se data fetch karna hai
-          let result = await fetch("/movies");
-          console.log(result); // result contains bhot saari chise,one of them is json data call by.json() which fetch function provides us
 
-          let json = await result.json();
-          console.log(json);
-      }
+  state = {
+    movies: [],
+    genre: []
+  }
 
-      f();
+  componentDidMount() { // data ek baar hi laana hai to hum data compDidMount() mein likhenge
+          // I will get data here
+          // we are in context of FrontEnd side => all Browsers exposed some functionalities like fetch()
+          // fetch() => which takes the link of file/API jha se data mangvaana hai
+          // fetch() is a async function
+          let f = async () => {
+            // let result = await fetch("http://localhost:4000/movies"); //API link jha se data fetch karna hai
+            // let result = await fetch("/movies");
+            // console.log(result); // result contains bhot saari chise,one of them is json data call by.json() which fetch function provides us
+
+            // let json = await result.json();
+            // console.log(json);
+            let responseGenre = await fetch("/genre");
+            let responseMovies = await fetch("/movies");
+            let moviesJson = await responseMovies.json();
+            let genreJson = await responseGenre.json();
+
+            this.setState({
+              movies: moviesJson,
+              genre: genreJson
+            });
+          }
+
+          f();
   }
 
   render() {
     return (
-        <div></div>
+      <div>
+
+        <Navbar />
+        
+        <div className="row">
+          <Filter genreData={this.state.genre}/>
+
+        </div>
+      </div>
     );
   }
 }
