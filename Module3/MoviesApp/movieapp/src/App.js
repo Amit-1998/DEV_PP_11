aabae9343fb28909ba5acba1d9252a24b6651fcd
Amit-1998 <amitkumar654521@gmail.com>
2021-08-12 14:19:07 +0530

    Proxy vaali chis samajh gaye

--- Module3/MoviesApp/movieapp/src/App.js
+++ Module3/MoviesApp/movieapp/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 
 class App extends React.Component {
   
-  componentDidMount(){
+  componentDidMount(){ // data ek baar hi laana hai to hum data compDidMount() mein likhenge
       // I will get data here
       // we are in context of FrontEnd side => all Browsers exposed some functionalities like fetch()
       // fetch() => which takes the link of file/API jha se data mangvaana hai
       // fetch() is a async function
       let f = async () => {
-          let result = await fetch("./data.json");
+          // let result = await fetch("http://localhost:4000/movies"); //API link jha se data fetch karna hai
+          let result = await fetch("/movies");
           console.log(result); // result contains bhot saari chise,one of them is json data call by.json() which fetch function provides us
 
           let json = await result.json();
