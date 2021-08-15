bfa20ba4677db400079e3b0d124af472c13319bc
Amit-1998 <amitkumar654521@gmail.com>
2021-08-15 21:38:42 +0530

    Routing done

--- Module3/MoviesApp/movieapp/src/App.js
+++ Module3/MoviesApp/movieapp/src/App.js
@@ -4,6 +4,12 @@ import Navbar from "./Navbar";
 import Pagination from "./Pagination";
 import Search from "./Search";
 import Table from "./Table";
+import Login from "./Login";
+import Customer from "./Customer";
+import Rentals from "./Rentals";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// ye teen chise nothing but three components
 
 class App extends React.Component {
   state = {
@@ -13,81 +19,99 @@ class App extends React.Component {
     search: ""
   }
 
-  updateSearch= (searchString)=>{
-      this.setState({ search: searchString});
+  updateSearch = (searchString) => {
+    this.setState({ search: searchString });
   }
 
-  setFilter = (filter)=>{
-      this.setState( {selectedFilter: filter} );
+  setFilter = (filter) => {
+    this.setState({ selectedFilter: filter });
   }
 
-   toggleLike = (id)=> {
-       let index = this.state.movies.findIndex((el)=>{
-            // for each el in arr
-            return el._id == id;
-       });
-       // make copy of moviesarr
-       let currMoviesArr = this.state.movies.map((el) => el);
-       if(currMoviesArr[index].liked){
-           currMoviesArr[index].liked = false;
-       }
-       else{
-          currMoviesArr[index].liked = true;
-       }
-       this.setState({movies: currMoviesArr});
-   }
+  toggleLike = (id) => {
+    let index = this.state.movies.findIndex((el) => {
+      // for each el in arr
+      return el._id == id;
+    });
+    // make copy of moviesarr
+    let currMoviesArr = this.state.movies.map((el) => el);
+    if (currMoviesArr[index].liked) {
+      currMoviesArr[index].liked = false;
+    }
+    else {
+      currMoviesArr[index].liked = true;
+    }
+    this.setState({ movies: currMoviesArr });
+  }
 
-   deleteMovie = (id)=>{
-       let filteredArr = this.state.movies.filter((el)=>{
-            return el._id != id;
-       });
+  deleteMovie = (id) => {
+    let filteredArr = this.state.movies.filter((el) => {
+      return el._id != id;
+    });
 
-       this.setState({movies: filteredArr});
-       
-   }
+    this.setState({ movies: filteredArr });
 
-   componentDidMount() { // data ek baar hi laana hai to hum data compDidMount() mein likhenge
-          // I will get data here
-          // we are in context of FrontEnd side => all Browsers exposed some functionalities like fetch()
-          // fetch() => which takes the link of file/API jha se data mangvaana hai
-          // fetch() is a async function
-          let f = async () => {
-            // let result = await fetch("http://localhost:4000/movies"); //API link jha se data fetch karna hai
-            // let result = await fetch("/movies");
-            // console.log(result); // result contains bhot saari chise,one of them is json data call by.json() which fetch function provides us
+  }
 
-            // let json = await result.json();
-            // console.log(json);
-            let responseGenre = await fetch("/genre");
-            let responseMovies = await fetch("/movies");
-            let moviesJson = await responseMovies.json();
-            let genreJson = await responseGenre.json();
+  componentDidMount() { // data ek baar hi laana hai to hum data compDidMount() mein likhenge
+    // I will get data here
+    // we are in context of FrontEnd side => all Browsers exposed some functionalities like fetch()
+    // fetch() => which takes the link of file/API jha se data mangvaana hai
+    // fetch() is a async function
+    let f = async () => {
+      // let result = await fetch("http://localhost:4000/movies"); //API link jha se data fetch karna hai
+      // let result = await fetch("/movies");
+      // console.log(result); // result contains bhot saari chise,one of them is json data call by.json() which fetch function provides us
 
-            this.setState({
-              movies: moviesJson,
-              genre: genreJson
-            });
-          }
+      // let json = await result.json();
+      // console.log(json);
+      let responseGenre = await fetch("/genre");
+      let responseMovies = await fetch("/movies");
+      let moviesJson = await responseMovies.json();
+      let genreJson = await responseGenre.json();
 
-          f();
+      this.setState({
+        movies: moviesJson,
+        genre: genreJson
+      });
+    }
+
+    f();
   }
 
   render() {
     return (
-      <div>
+      <Router>
+              <div>
+                    <Navbar />
 
-        <Navbar />
-        
-        <div className="row">
-              <Filter handleFilter={this.setFilter} selectedFilter={this.state.selectedFilter} genreData={this.state.genre}/>
-              
-              <div class="col-9 p-5">
-                   <Search search={this.state.search} updateSearch={this.updateSearch} totalMovies={this.state.movies.length} />
-                   <Table search={this.state.search} deleteMovie={this.deleteMovie} toggleHeart = {this.toggleLike} selectedFilter={this.state.selectedFilter} moviesData={this.state.movies}/>
-                   
+                    <Switch>
+                        <Route path="/customer">
+                            <Customer />
+                        </Route>
+
+                        <Route path="/rentals">
+                            <Rentals />
+                        </Route>
+                        
+                        <Route path="/login">
+                             <Login />
+                        </Route>
+                        
+                        <Route path="/">
+                                <div className="row">
+                                  <Filter handleFilter={this.setFilter} selectedFilter={this.state.selectedFilter} genreData={this.state.genre} />
+
+                                  <div class="col-9 p-5">
+                                    <Search search={this.state.search} updateSearch={this.updateSearch} totalMovies={this.state.movies.length} />
+                                    <Table search={this.state.search} deleteMovie={this.deleteMovie} toggleHeart={this.toggleLike} selectedFilter={this.state.selectedFilter} moviesData={this.state.movies} />
+
+                                  </div>
+                                </div>
+                        </Route>
+                    </Switch>
               </div>
-        </div>
-      </div>
+      </Router>
+
     );
   }
 }
--- Module3/MoviesApp/movieapp/src/Navbar.jsx
+++ Module3/MoviesApp/movieapp/src/Navbar.jsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
+// Link is a component of "react-router-dom"
+// Link is a anchor Component of "react-router-dom" 
+// agar ham routing use kar rhe hai and hame hyperlinks create karne hai to ham anchor tag use nhi karenge instead we will replace them by Link tags
+
+// Link tag href attribute nhi leta , instead of that it take "to" as attribute 
 let Navbar = () => {
 
     return (
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">MoviesRental</a>
+                <Link class="navbar-brand" to="/">MoviesRental</Link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -11,16 +17,16 @@ let Navbar = () => {
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <Link class="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Customers</a>
+                            <Link class="nav-link" to="/customer">Customers</Link>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Rentals</a>
+                            <Link class="nav-link" to="/rentals">Rentals</Link>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link " href="#" tabindex="-1">Login</a>
+                            <Link class="nav-link " to="/login" tabindex="-1">Login</Link>
                         </li>
                     </ul>
                 </div>
