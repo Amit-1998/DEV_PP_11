c576af37f22e9ba32bd5aba2c44823d157be6466
Amit-1998 <amitkumar654521@gmail.com>
2021-08-17 17:16:42 +0530

    Learn how to integrate firebase

---
+++ Module3/context-api/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAmn7FeEs2FJa43MibI_SDgV6UTd2oQwB4",
+    authDomain: "test-a7359.firebaseapp.com",
+    projectId: "test-a7359",
+    storageBucket: "test-a7359.appspot.com",
+    messagingSenderId: "470824982328",
+    appId: "1:470824982328:web:8ac9412284e2ccc08453cd"
+  };
+
+firebase.initializeApp(firebaseConfig);
