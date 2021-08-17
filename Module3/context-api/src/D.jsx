d90937f198831e4a03aede0ba39585c45b851327
Amit-1998 <amitkumar654521@gmail.com>
2021-08-17 16:47:33 +0530

    context-api ke  steps done

---
+++ Module3/context-api/src/D.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+//step 5: import the context you exported in the component where you want to get the state
+import { countContext } from "./A";
+
+
+//mujhe is comp vo Provide ke props value use karni hai
+let D = ()=> {
+    // iske ander useContext use karenge
+
+    //step 6: inside the functional component call useContext and give it the context
+    // from which you want the value
+    let value = useContext(countContext); // ye vo context leta hai jo hamne A comp mein bnaya the
+    // jo hamne Provider comp ko value provide kari thi vo 'value' ko mil jayegi
+    return (
+      <div className="d-wala-div">
+          {value}
+      </div>
+    );
+  }
+  
+  export default D;
+  
