eb5d778d487c730e2edf6a45305aa9e3ef052d1a
Amit-1998 <amitkumar654521@gmail.com>
2021-08-20 14:33:34 +0530

    commentbox box css,user photo,his name, his comment all styling done

--- Module3/reels/src/Components/videoCard.jsx
+++ Module3/reels/src/Components/videoCard.jsx
@@ -4,7 +4,7 @@ import "./videoCard.css"
 let VideoCard = () => {
 
     let [playing, setPlaying] = useState(false);
-    
+    let [commentBoxOpen, setCommentBoxOpen] = useState(false);
 
     return (
         <div className="video-card">
@@ -15,11 +15,39 @@ let VideoCard = () => {
                 <marquee>some song</marquee>
             </span>
 
-            <span class="material-icons-outlined video-card-comment">chat</span>
+            <span onClick={(e)=>
+                    { if(commentBoxOpen){
+                          setCommentBoxOpen(false);
+                        }
+                      else{
+                        setCommentBoxOpen(true);
+                      }
+                    }
+                  }
+              class="material-icons-outlined video-card-comment">chat
+            </span>
 
             <span class="material-icons-outlined video-card-like">favorite_border</span>
             
-            
+            {commentBoxOpen ? 
+                <div className="video-card-comment-box">
+                    <div className="actual-comments">
+                       <div className="post-user-comment">
+                           <img src="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"/>
+                           <div>
+                                <h5>user name</h5>
+                                <p>this is user's comments</p>
+                           </div>
+                       </div>
+                       
+                    </div>
+                    <div className="comment-form">
+                        <input type="text"/>
+                        <button>Post</button>
+                    </div>
+                </div> : ""
+            }
+
             <video onClick={(e) => {
                 if (playing) {
                     e.currentTarget.pause();
