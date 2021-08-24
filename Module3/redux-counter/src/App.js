6736ee2529f2482b0b266ce4f41ee2649b7e4a89
Amit-1998 <amitkumar654521@gmail.com>
2021-08-24 15:16:31 +0530

    button onClick dispatch & concept of Increment action

--- Module3/redux-counter/src/App.js
+++ Module3/redux-counter/src/App.js
@@ -1,13 +1,36 @@
+import { useContext } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+// import the actions
+import { incrementCreator } from "./redux/actions"; // incrementCreator is an action
+
+
 let App = () => {
 
   // Provider component ke store se state nikal kar ham us state ko change kar sakte 
-  // for this work we have two hooks
-  
+  // for this work we have two hooks 1) useSelector() 2) useDispatch()
+
+  // useSelector(function(){}) takes a function and is function ko vo saari states milti milti hai jo store mein hoti hai
+  // useSelector() ke ander function jo paas hua hai vo state leta hai and ham state ke saath kuch bhi kar sakte hai sirf update ko chod kar
+  // jo chis (state) ander passing function se return vhi same state useSelector se bhi return hoti hai
+
+  // let state = useSelector( function (state){
+  //          return state;
+  //     });
+
+ let state = useSelector( (state) => state );
+
+ // another hook useDispatch()
+ let dispatch = useDispatch(); // useDispatch() hook ko call karne se we get a function named "dispatch"
+ // ham dispatch function ki madad se jitne bhi hamare actions hain unko dispatch kar sakte hai
+
 
   return (
     <>
-       <button>+</button>
-       <p>0</p>
+       <button onClick={() => {
+           dispatch(incrementCreator());
+       }}>+</button>
+       <p>{state}</p>
     </>
   );
 }
--- Module3/redux-counter/src/redux/reducer.js
+++ Module3/redux-counter/src/redux/reducer.js
@@ -2,7 +2,7 @@
 
 // reducer funct => takes two parameters => 1) state with initialize some value, 2) action
 
-let reducer = (state = 0, action)=>{
+let reducer = (state = 0, action)=>{ // hame nhi pta par khi se iske paas action object aayega
      
     switch(action.type){
         case "INCREMENT":
