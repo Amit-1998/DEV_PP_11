c56755ea1bb9674233d5fdb6926f5220640f5d6e
Amit-1998 <amitkumar654521@gmail.com>
2021-08-25 15:49:02 +0530

    preview page setUp

--- Module3/shopping-cart/src/components/navbar.jsx
+++ Module3/shopping-cart/src/components/navbar.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import "./navbar.css";
 
 let Navbar = () => {
     return (
         <>
-            <nav class="navbar navbar-dark bg-dark">
+            <nav class="navbar navbar-dark bg-dark giveHeighttoIt">
                 <div class="container-fluid">
                     <Link class="navbar-brand" to="/">Shopping Cart</Link>
                 </div>
--- Module3/shopping-cart/src/components/preview.jsx
+++ Module3/shopping-cart/src/components/preview.jsx
@@ -1,7 +1,20 @@
+import "./preview.css";
+
 let Preview = ()=>{
     return (
         <>
-           preview
+          <div className="preview-container">
+                <div className="preview-img-container">
+                    <img src="https://images-eu.ssl-images-amazon.com/images/I/71sxlhYhKWL._AC._SR360,460.jpg"  />
+                </div>    
+                
+                <div className="preview-listing">
+                    <h2>Title</h2>
+                    <p>What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?
+                    </p>
+                    <button>Add to Cart</button>
+                </div>
+          </div>
         </>
     );
 }
