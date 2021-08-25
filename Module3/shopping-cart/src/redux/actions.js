0a6f22afa73c731f8978acf2f649988e577524e1
Amit-1998 <amitkumar654521@gmail.com>
2021-08-25 18:03:13 +0530

    Shopping cart => actions and reducer done

---
+++ Module3/shopping-cart/src/redux/actions.js
@@ -0,0 +1,16 @@
+import { ADD_TO_CART, REMOVE_FROM_CART } from "./constants"
+
+// add action OR addCreator
+export const addCreator = ()=>{ // action Creator jo sirf btayega ki kis product ko cart mein add krana hai
+     return{
+         type: ADD_TO_CART,
+         payload: id
+     }
+}
+
+export const removeCreator = (id)=>{ // action Creator jo sirf btayega ki kis product ko cart se remove krana hai
+    return{
+        type: REMOVE_FROM_CART,
+        payload: id
+    }
+}
---
+++ Module3/shopping-cart/src/redux/reducers.js
@@ -0,0 +1,57 @@
+import { ADD_TO_CART, REMOVE_FROM_CART } from "./constants"
+
+// initialState is an array of objects(products)
+let initialState = [  
+    {
+        id: 0,
+        name: "Palm Phone",
+        img = "https://images-eu.ssl-images-amazon.com/images/I/71sxlhYhKWL._AC._SR360,460.jpg",
+        price: 10000,
+        qty: 0
+
+    },
+
+    {
+        id: 1,
+        name: "Kia Car",
+        img = "https://media.zigcdn.com/media/content/2019/Aug/2019_kia_seltos_fd_goa_zigwheels_1_640x480.jpg",
+        price: 1200000,
+        qty: 0
+
+    },
+
+    {
+        id: 2,
+        name: "BMW Bike",
+        img = "https://ic1.maxabout.us/autos/tw_india//D/2021/3/dhoom-3-bike-bmw-k1300r.jpg",
+        price: 300000,
+        qty: 0
+
+    }
+
+]
+
+let reducer = ( state = initialState, action) => {
+     
+     switch(action.type){
+         
+         case ADD_TO_CART:
+             let cp = state.map((el) => el);
+             let id = action.payload;
+             cp[id].qty = cp[id].qty + 1;
+             return cp;
+
+         case REMOVE_FROM_CART:
+             let cp = state.map((el) => el);
+             let id = action.payload;
+             if(cp[id].qty > 0)
+                cp[id].qty = cp[id].qty - 1;
+             return cp;
+
+
+         default: 
+            return state;
+     }
+};
+
+export default reducer;
