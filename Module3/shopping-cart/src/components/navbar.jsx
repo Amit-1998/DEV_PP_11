483095a79f5f5fd2d01981492be5f6c0d7fc061d
Amit-1998 <amitkumar654521@gmail.com>
2021-08-25 10:43:06 +0530

    replace anchor tag with Link tag to go on Home page on clicking of Shopping Cart written on Navbar

--- Module3/shopping-cart/src/components/navbar.jsx
+++ Module3/shopping-cart/src/components/navbar.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 let Navbar = () => {
     return (
         <>
             <nav class="navbar navbar-dark bg-dark">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Shopping Cart</a>
+                    <Link class="navbar-brand" to="/">Shopping Cart</Link>
                 </div>
             </nav>
 
