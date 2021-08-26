f9c4d7b859c23ea6f8bd0eafbfb9a3c99c60ec93
Amit-1998 <amitkumar654521@gmail.com>
2021-08-26 13:43:28 +0530

    filtered Arr contains only products whose qty>0

--- Module3/shopping-cart/src/components/cart.jsx
+++ Module3/shopping-cart/src/components/cart.jsx
@@ -1,33 +1,26 @@
+import { useSelector } from "react-redux";
+
 let Cart = () => {
+
+    let state = useSelector(state=>state)
+    console.log(state);
+
+    let filteredArr = state.filter((el) => el.qty > 0);
+
     return (
         <>
             <table class="table">
                 <thead>
                     <tr>
                         <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
+                        <th scope="col">Product Name</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Qty</th>
+                        <th scope="col">Amount</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td colspan="2">Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
+                    
                 </tbody>
             </table>
         </>
