83daf4d960dc50aeefeb9f1d010ec1a51be02392
Amit-1998 <amitkumar654521@gmail.com>
2021-08-29 11:44:56 +0530

    Resume Builder routing, firebase integrate

---
+++ Module3/resume-builder/src/App.js
@@ -0,0 +1,31 @@
+import Login from "./components/login";
+import Navbar from "./components/navbar";
+import SignUp from "./components/signup";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/home";
+
+let App = ()=>{
+  return (
+    <>
+       <Router>
+           <Navbar />
+           <Switch>
+              <Route path="/login">
+                 <Login />
+              </Route>
+              
+              <Route path="/signup">
+                  <SignUp />
+              </Route>
+              
+              <Route path="/">
+                  <Home />
+              </Route>
+           </Switch>
+       </Router>  
+    </>
+  );
+}
+
+export default App;
---
+++ Module3/resume-builder/src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCzZgYKfpxoHED12JXkKSGOMSGBfzg2LGU",
+    authDomain: "resume-builder-fab5f.firebaseapp.com",
+    projectId: "resume-builder-fab5f",
+    storageBucket: "resume-builder-fab5f.appspot.com",
+    messagingSenderId: "999542914181",
+    appId: "1:999542914181:web:92301c9d978a45e0793dec"
+  };
+
+firebase.initializeApp(firebaseConfig);
