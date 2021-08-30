34ddf7b13f04dce68bac8efeaa41846a51cef4e9
Amit-1998 <amitkumar654521@gmail.com>
2021-08-30 16:35:57 +0530

    gives code to each template

--- Module3/resume-builder/src/components/home.jsx
+++ Module3/resume-builder/src/components/home.jsx
@@ -1,13 +1,18 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { auth } from "../firebase";
 
 import "./css/home.css"
+import { templateCreator } from "../redux/actions/templateActions"
+
 
 let Home = ()=>{
 
     let user = useSelector( (state) => state.user);
     // console.log(user);
+    let code = useSelector((state)=>state.template);
+    console.log(code);
+
     let dispatch = useDispatch();
 
     return (
@@ -15,11 +20,11 @@ let Home = ()=>{
            { user?"Home" : <Redirect to="/login" /> }
 
             <div className="template-container">
-                <div className="template">
+                <div onClick={()=>{ dispatch(templateCreator("A")); }} className="template">
                     <img src="http://localhost:3000/skin1.svg"></img>
                 </div>
                 
-                <div className="template">
+                <div onClick={()=>{ dispatch(templateCreator("B")); }} className="template">
                      <img src="http://localhost:3000/skin2.svg"></img>
                 </div>
             </div>
