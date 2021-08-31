9ab340bfb2e7d1fb89cf011e19a768a559f04a96
Amit-1998 <amitkumar654521@gmail.com>
2021-08-31 13:48:56 +0530

    PreviewComp : extract all states and template Code for template

--- Module3/resume-builder/src/components/preview.jsx
+++ Module3/resume-builder/src/components/preview.jsx
@@ -1,6 +1,12 @@
+import { useSelector } from "react-redux";
 import "./css/preview.css";
 
 let Preview = ()=>{
+
+    let {fname, lname, city, state, email, phone, cgpa, college, degree, year}  = useSelector((state)=>state.details);
+    let templateCode = useSelector((state)=>state.template);
+    
+    
     return (
         <>
             <div className="preview-container">
