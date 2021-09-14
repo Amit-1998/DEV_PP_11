6ef9a360e3caa0028f2cd9e31e6cca05837f5a50
Amit-1998 <amitkumar654521@gmail.com>
2021-09-14 16:09:49 +0530

    /user

--- Module4 (Backend)/foodApp/app.js
+++ Module4 (Backend)/foodApp/app.js
@@ -1,3 +1,7 @@
+// npm init -y
+// npm install express
+// npm i nodemon -g
+
 const express = require("express");
 
 const app = express(); // issse hmara server create ho gya // express hame ek function deta hai app name se
@@ -8,20 +12,37 @@ app.listen(port, function(){ // server ko btane ke lie ki ham is port par kaam k
     console.log(`Server is listening on port No : ${port}`);
 });
 
+
+app.get('/', (req,res)=>{
+    console.log(req.hostname); 
+    console.log(req.path);  
+    console.log(req.method); 
+    console.log("hello from home page");
+    res.send('<h1>hi hello from Backend</h1>'); // res object itna smart hai vo is passing html ko body mein embed bhi kar dega
+});
+
 //types of request -> get, post, put, delete
-app.get('/home', (req,res)=>{ // client ne jab '/home' route par request maari tab
-    // request to mangvaali '/' is route se
-    // console.log(req); // req is a big object
-    // request object object mein chise hmare paas aati hai
-    // response object mein ham chise bhejte hain
-    console.log(req.hostname); // hostname would be localhost
-    console.log(req.path);  // path would be /home
-    console.log(req.method); // method is GET
+// app.get('/home', (req,res)=>{ // client ne jab '/home' route par request maari tab
+//     // request to mangvaali '/' is route se
+//     // console.log(req); // req is a big object
+//     // request object object mein chise hmare paas aati hai
+//     // response object mein ham chise bhejte hain
+//     console.log(req.hostname); // hostname would be localhost
+//     console.log(req.path);  // path would be /home
+//     console.log(req.method); // method is GET
 
-    // ab response bhi to bhejna padega
-    res.send('<h1>hello</h1>');
-    // jab hame apne server ka response khatam karna ho to uske liye res.end()
-    res.end();
+//     // ab response bhi to bhejna padega
+//     res.send('<h1>hello</h1>');
+//     // jab hame apne server ka response khatam karna ho to uske liye res.end()
+//     res.end();
 
-})
+// })
 
+let obj = { 'name': "Amit" }
+
+// how to send object as a response
+app.get('/user', (req, res)=>{
+    //  res.send(obj);
+     // agar hmare paas json data hai to
+     res.json(obj);
+});
