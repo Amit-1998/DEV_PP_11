17f7bf739f1fcd40988e16e6586499f69e3920dc
Amit-1998 <amitkumar654521@gmail.com>
2021-09-15 11:24:13 +0530

    patch and delete requests

--- Module4(Backend)/foodApp/server.js
+++ Module4(Backend)/foodApp/server.js
@@ -21,20 +21,20 @@ app.get('/', (req, res)=>{
 
 app.get('/user', (req, res)=>{
     // res.send('user');
-    res.json(user); // json format mein data mangvaaya
+    res.json(user); // json format mein data mangvaaya server se
 });
 
 // post request -> koi bhi data bhejne ke liye we use post request 
 // client -> server (when client sends data to the server )
 
 app.post('/user', (req, res)=>{
-    user = req.body;
+    user = req.body; // server ke paas req object mein milta hai data jo frontend se client ne bheja hota hai
     // console.log(req.body);
     res.send('data has been added successfully'); // server se client par message bhejne ke liye res.send()
 
 });
 
-// patch request method -> koi bhi chis update karne ke liye
+// patch request method -> koi bhi chis update karne ke liye client -> server
 app.patch('/user', (req, res)=>{
       // user = req.body;
       // console.log(user); // { age: '10' }
@@ -46,7 +46,7 @@ app.patch('/user', (req, res)=>{
            // user[key] ye dekhega ki mere paas to "key" as a key to hai hi nhi to vo sochega acha ye koi variable hai jisme "age" hai to apne ander "age" ko as a key dekhega vo bhi nhi milega to bna dega
       }
       // res.send("data has been updated successfully");
-      res.json(user);
+      res.json(user); // {"name":"Amit Kumar","age":"10"}
 });
 
 // rough work
@@ -60,5 +60,9 @@ app.patch('/user', (req, res)=>{
 //     // user[key] ye dekhega ki mere paas to "key" as a key to hai hi nhi to vo sochega acha ye koi variable hai jisme "age" hai to apne ander "age" ko as a key dekhega vo bhi nhi milega to bna dega
 // }
 
-
+// delete request -> used to delete the user
+app.delete('/user', (req, res)=>{
+    user = {};
+    res.json(user);
+});
 
