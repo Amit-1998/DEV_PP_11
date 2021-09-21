4caa12e90888e52df57ba61add17f29e7e978811
Amit-1998 <amitkumar654521@gmail.com>
2021-09-21 16:14:37 +0530

    Don't push dblink link

--- Module4(Backend)/foodApp/models/userModel.js
+++ Module4(Backend)/foodApp/models/userModel.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 // const db_link = 'mongodb+srv://AmitfoodApp:<password>@cluster0.lwgl1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const db_link = 'mongodb+srv://AmitfoodApp:SeeBa5L6SDwmEHiR@cluster0.lwgl1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// const db_link = 'mongodb+srv://AmitfoodApp:SeeBa5L6SDwmEHiR@cluster0.lwgl1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 // copied password => SeeBa5L6SDwmEHiR
+
+const validator = require("email-validator");
 
 mongoose.connect(db_link).then(function(){
     console.log('db connected');
@@ -17,10 +19,17 @@ const userSchema = new mongoose.Schema({
         required: true
     },
 
+    age: {
+        type: Number
+    }
+
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        validate: function(){
+            return validator.validate(this.email);
+        }
     },
 
     password: {
@@ -32,7 +41,10 @@ const userSchema = new mongoose.Schema({
     confirmPassword: {
         type: String,
         required: true,
-        min: 8
+        min: 8,
+        validate: function(){
+            return this.password==this.confirmPassword;
+        }
     }
 
 });
