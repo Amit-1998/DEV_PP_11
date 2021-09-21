8d49295b9db118549e370fa5330b4bdca55f236a
Amit-1998 <amitkumar654521@gmail.com>
2021-09-21 17:11:51 +0530

    secure out db_link

--- Module4(Backend)/foodApp/models/userModel.js
+++ Module4(Backend)/foodApp/models/userModel.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 // const db_link = 'mongodb+srv://AmitfoodApp:<password>@cluster0.lwgl1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 // const db_link = 'mongodb+srv://AmitfoodApp:SeeBa5L6SDwmEHiR@cluster0.lwgl1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-
+const db_link = require('../secrets');
 // copied password => SeeBa5L6SDwmEHiR
 
 const validator = require("email-validator");
 
-mongoose.connect(db_link).then(function(){
+mongoose.connect(db_link).then(function(db){
+    // console.log(db);
     console.log('db connected');
 })
 .catch(function(err){
@@ -21,7 +22,7 @@ const userSchema = new mongoose.Schema({
 
     age: {
         type: Number
-    }
+    },
 
     email: {
         type: String,
@@ -48,3 +49,17 @@ const userSchema = new mongoose.Schema({
     }
 
 });
+
+const userModel = mongoose.model('userModel', userSchema);
+
+(async function createUser(){
+    let user={
+        name: 'Abhi',
+        age: 20,
+        email: 'abc@gmail.com',
+        password: '12345678',
+        confirmPassword: '12345678'
+    };
+   let userObj = await userModel.create(user);
+   console.log(userObj);
+})();
