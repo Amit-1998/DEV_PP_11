b0c2b420cf784a9fd0d90ea6c9c8b6b889d41c2c
Amit-1998 <amitkumar654521@gmail.com>
2022-02-23 08:15:15 +0530

    product SChema bnaya than uska model bna kar export kar dia

--- MERN project/backend/models/productModel.js
+++ MERN project/backend/models/productModel.js
@@ -3,7 +3,8 @@
  const productSchema = new mongoose.Schema({
      name:{
          type: String,
-         required: [true,"Please Enter product's Name"]
+         required: [true,"Please Enter product's Name"],
+         trim: true
      },
      description:{
          type: String,
@@ -13,5 +14,58 @@
          type: Number,
          required: [true,"Please Enter product's price"],
          maxlength: [8, "Price cannot exceeds 8 figures"]
+     },
+     rating:{
+         type: Number,
+         default: 0
+     },
+     images: [
+        {
+            public_id:{
+                type: String,
+                required: true
+            },
+            url:{
+                type: String,
+                required: true
+            }
+        }
+     ],
+     category:{
+         type: String,
+         required: [true, "Please Enter product Category"],
+     },
+     Stock:{
+         type: Number,
+         required: [true,"Please Enter product Stock"],
+         maxlength: [4,"Stock cannot exceed 4 figures"],
+         default: 1
+     },
+     noOfReviews:{
+         type: Number,
+         default: 0
+     },
+     reviews:[
+         {
+            name:{
+                type: String,
+                required: true,
+            },
+            rating:{
+                type: Number,
+                required: true,
+            },
+            comment:{
+                type: String,
+                required: true
+            } 
+         }
+     ],
+     createdAt:{
+         type:Date,
+         default: Date.now
      }
+
  })
+
+ module.exports = mongoose.model("Product",productSchema);
