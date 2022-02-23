18a72099cb3613033b17021c0d0b52b1b312630a
Amit-1998 <amitkumar654521@gmail.com>
2022-02-23 19:41:24 +0530

    create the product

--- MERN project/backend/controllers/productController.js
+++ MERN project/backend/controllers/productController.js
@@ -1,3 +1,14 @@
+const Product = require("../models/productModel");
+
+// Create Product
+module.exports.createProduct = async (req,res,next)=>{
+    const product = Product.create(req.body);
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
+
 module.exports.getAllProducts = (req,res)=>{
     res.status(200).json({
         message: "Route is working fine"
